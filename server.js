952952197b0ef0308046c105c6da4151d3b26a78
Devi <rmdn321@gmail.com>
2022-10-08 17:57:14 -0700

    Added routes for write a new note to db

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const path = require('path');
 const fs = require('fs');
 const util = require('util');
 
+const uuid = require('uuid');
+
 const PORT = process.env.PORT || 3001;
 
 const app = express();
@@ -19,6 +21,35 @@ app.use(express.static('public'));
 
 // Promise version of fs.readFile
 const readFromFile = util.promisify(fs.readFile);
+
+/**
+ *  Function to read data from a given a file and append some content
+ *  @param {object} content The content you want to append to the file.
+ *  @param {string} file The path to the file you want to save to.
+ *  @returns {void} Nothing
+ */
+ const readAndAppend = (content, file) => {
+  fs.readFile(file, 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      const parsedData = JSON.parse(data);
+      parsedData.push(content);
+      writeToFile(file, parsedData);
+    }
+  });
+};
+
+/**
+ *  Function to write data to the JSON file given a destination and some content
+ *  @param {string} destination The file you want to write to.
+ *  @param {object} content The content you want to write to the file.
+ *  @returns {void} Nothing
+ */
+ const writeToFile = (destination, content) =>
+ fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
+   err ? console.error(err) : console.info(`\nData written to ${destination}`)
+ );
 
 ////////////////
 // Routes
@@ -38,6 +69,26 @@ app.get('/notes', (req, res) => {
 app.get('/api/notes', (req, res) => {
   console.info(`${req.method} request received for notes`);
   readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
+
+// POST Route for a new note
+app.post('/api/notes', (req, res) => {
+  console.info(`${req.method} request received to add a note`);
+
+  const { title, text } = req.body;
+
+  if (req.body) {
+    const newNote = {
+      title,
+      text,
+      note_id: uuid.v4(),
+    };
+
+    readAndAppend(newNote, './db/db.json');
+    res.json(`Note added successfully`);
+  } else {
+    res.error('Error in adding note');
+  }
 });
 
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
