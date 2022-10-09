65198a97f71fc3f261276a9a317f8dbb1fa498c0
Devi <rmdn321@gmail.com>
2022-10-08 17:52:03 -0700

    Added html routes for front end

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const path = require('path');
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+////////////////
+// Routes
+////////////////
+
+// GET Route for *
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+});
+
+// GET Route for *
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+});
+
+app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
