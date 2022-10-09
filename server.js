89fc745f91364b43dd5eab7d18cfa5582d958ff6
Devi <rmdn321@gmail.com>
2022-10-08 17:55:09 -0700

    Added routes for getting all notes from db

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
+const util = require('util');
 
 const PORT = process.env.PORT || 3001;
 
@@ -10,6 +12,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
+
+//////////////////////////
+// Helper Functions 
+/////////////////////////
+
+// Promise version of fs.readFile
+const readFromFile = util.promisify(fs.readFile);
 
 ////////////////
 // Routes
@@ -23,6 +32,12 @@ app.get('/', (req, res) => {
 // GET Route for *
 app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, '/public/notes.html'))
+});
+
+// GET Route for Notes
+app.get('/api/notes', (req, res) => {
+  console.info(`${req.method} request received for notes`);
+  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
 
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
